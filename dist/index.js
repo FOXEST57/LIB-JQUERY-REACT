5d21f44ff284a9471cef47efcd95216fbc15b846
FOXEST57 <foxest57@gmail.com>
2023-03-21 13:31:30 +0100

    modif

--- dist/index.js
+++ dist/index.js
@@ -3,21 +3,11 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-Object.defineProperty(exports, "Badge", {
+Object.defineProperty(exports, "Modal", {
   enumerable: true,
   get: function get() {
-    return _Badge.default;
+    return _Modal.default;
   }
 });
-Object.defineProperty(exports, "Button", {
-  enumerable: true,
-  get: function get() {
-    return _Button.default;
-  }
-});
-
-var _Badge = _interopRequireDefault(require("./components/Badge"));
-
-var _Button = _interopRequireDefault(require("./components/Button"));
-
+var _Modal = _interopRequireDefault(require("./components/Modal"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
